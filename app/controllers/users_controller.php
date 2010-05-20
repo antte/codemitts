9dc68ec69bf875dc454cbfc71937c41f2121c84b
antte <andreas.fliesberg@gmail.com>
2010-05-20 21:47:00 +0200

    login process

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -1,10 +1,23 @@
 <?php
+	
 	class UsersController extends AppController {
 		
 		/**
 		 * Login action for the login view
 		 */
 		function login(){
-			
+			if($this->data) {
+				
+				$verifies =
+				$this->User->verify($this->data['User']['username'], $this->data['User']['password']);
+				
+				if($verifies) {
+					$this->Session->write("loggedIn", 1);
+					$this->redirect(array('action' => 'index'));
+				} else {
+					$this->Session->setFlash("Wrong username or password");
+				}
+				
+			}
 		}
 	}
