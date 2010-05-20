005b269aedb3df89733cb1b809d11ddf5e027e88
antte <andreas.fliesberg@gmail.com>
2010-05-20 22:20:42 +0200

    login..seems to work

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -3,7 +3,7 @@
 	class UsersController extends AppController {
 		
 		function beforeFilter() {
-			if(!$this->Session->check("loggedIn")) {
+			if(!$this->Session->read("loggedIn")) {
 				if (!$this->actionIsPermitted($this->params['action']))
 					//how horribly will this fail if login isnt one of the permitted actions :D
 					$this->redirect(array('action' => 'login'));
@@ -21,6 +21,7 @@
 				
 				if($verifies) {
 					$this->Session->write("loggedIn", 1);
+					$this->Session->setFlash("You are now logged in.");
 					$this->redirect(array('action' => 'index'));
 				} else {
 					$this->Session->setFlash("Wrong username or password");
@@ -35,8 +36,26 @@
 		 */
 		function actionIsPermitted($action) {
 			//hilol :d
-			if ($action == "login") return true;
+			if 		($action == "login") return true;
+			//elseif 	($action == "create") return true;
 			else return false;
+		}
+		
+		/**
+		 * TODO debug function not intended for deploy 
+		 */
+		function create($username, $password) {
+			
+			if(Configure::read("debug") < 1) return;
+			
+			$user['User']['username'] = $username;
+			$user['User']['password'] = md5($password);
+			
+			if($this->User->save($user)) {
+				$this->Session->setFlash("great success");
+			} else {
+				$this->Session->setFlash("fail!");
+			}
 		}
 		
 	}
--- app/models/user.php
+++ app/models/user.php
@@ -1,4 +1,20 @@
 <?php
 	class User extends AppModel {
 		
+		/**
+		 * Checks if its a valid user
+		 * @param $username
+		 * @param $password
+		 */
+		function verify($username, $password) {
+			
+			$user = $this->findByUsername($username);
+			
+			if($user['User']['password'] == md5($password))
+				return true;
+			else
+				return false;
+			
+		}
+		
 	}
