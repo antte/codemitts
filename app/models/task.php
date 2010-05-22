c6d6e97fce8cfb5d23dbca1d94eb2c8cfa1a1be2
antte <andreas.fliesberg@gmail.com>
2010-05-22 21:21:20 +0200

    in progress

--- app/models/task.php
+++ app/models/task.php
@@ -1,4 +1,19 @@
 <?php
 	class Task extends AppModel {
+		
 		var $belongsTo = 'Project';
+		
+		/**
+		 * Given a taskType return a random task
+		 * @param $taskType
+		 * @return a task
+		 */
+		function random($taskType) {
+
+			$task = array();
+			
+			return $task;
+			
+		}
+		
 	}
